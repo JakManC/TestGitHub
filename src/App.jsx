c74624ccd6c6ece0acaaa303be4623fdea65aab8
JakManC <mangeshgchaudhari75@gmail.com>
2025-11-23 05:39:34 +0530

    InitialFile
    GroundLevel

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1
+        className="
+      bg-orange-700 text-white p-2 m-2 rounded-full text-center
+      "
+      >
+        This Is Initial Counter File....
+      </h1>
+    </div>
+  );
+}
+
+export default App;
